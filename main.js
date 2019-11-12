175466f8ceaa278be339eb1f380b7c7e6d20e5b4
JoeyJr37 <JoeyJr11@gmail.com>
2019-11-12 06:25:18 -0600

    updated logic and added UI

--- main.js
+++ main.js
@@ -2,6 +2,60 @@
 /* eslint-disable no-loop-func */
 /* eslint-disable no-alert */
 /* eslint-disable quotes */
+
+/* Button to reveal game */
+let gamePlayButton = document.querySelector("#gamePlayButton");
+let gameButton = document.querySelector("#game");
+const revealGame = () => {
+  gameButton.style.visibility = "visible";
+};
+gamePlayButton.addEventListener("click", revealGame);
+
+let playerDisplayScore = document.querySelector("#playerScore");
+let computerDisplayScore = document.querySelector("#computerScore");
+
+/* set playerScore and computerScore to 0 */
+playerDisplayScore.value = 1;
+computerDisplayScore.value = 1;
+
+/* function to determine winner (first to 5 points) */
+const determineWinner = () => {
+  if (playerDisplayScore.textContent === "5") {alert("Player 1 wins!");}
+  else if (computerDisplayScore.textContent === "5") {alert ("Computer wins!");}
+  else {alert(`Player has ${playerDisplayScore.value - 1} and Computer has ${computerDisplayScore.value - 1}`);};
+  };
+
+/* function that sets validatedPlayerSelection to rock */
+const rockChoice = () => {
+  const validatedPlayerSelection = "Rock";
+  oneRound(computerPlay(), validatedPlayerSelection);
+  determineWinner();
+};
+
+/* function that sets validatedPlayerSelection to paper */
+const paperChoice = () => {
+  const validatedPlayerSelection = "Paper";
+  oneRound(computerPlay(), validatedPlayerSelection);
+  determineWinner();
+};
+
+/* function that sets validatedPlayerSelection to scissors */
+const scissorChoice = () => {
+  const validatedPlayerSelection = "Scissors";
+  oneRound(computerPlay(), validatedPlayerSelection);
+  determineWinner();
+};
+
+/* These are buttons for RPS game v2 */
+const rockButton = document.querySelector("#rockButton");
+rockButton.addEventListener("click", rockChoice);
+
+const paperButton = document.querySelector("#paperButton");
+paperButton.addEventListener("click", paperChoice);
+
+const scissorButton = document.querySelector("#scissorButton");
+scissorButton.addEventListener("click", scissorChoice);
+
 const computerPlay = () => {
   const computerValue = Math.floor((Math.random()) * 3);
   let computerChoice;
@@ -18,91 +72,44 @@ const computerPlay = () => {
   return computerChoice;
 };
 
-/* this is the main function that completes a 5 round game of Rock, Paper 
-
-Scissors, keeps the scores of each player, validates the user input and declares 
-
-a winner */
-
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-
 /*this function adds a point to the player score after a victory */
-  const win = () => {
-    playerScore++;
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-  };
+const win = () => {
+  /* playerScore++;
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`); */
+  playerDisplayScore.textContent = playerDisplayScore.value++;
+};
 
 /*this function adds a point to the computer score after a player loss */
-  const lose = () => {
-    computerScore++;
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-  };
-
-/* this function validates the user Input */
-const validate = (userInput) => {
-userInput = prompt("Rock, Paper or Scissors?");
-let lowerCaseUserInput = userInput.toLowerCase();
-let firstLetterCapitalized = lowerCaseUserInput.charAt(0).toUpperCase();
-let restOfWord = lowerCaseUserInput.slice(1);
-let validatedInput = firstLetterCapitalized + restOfWord;
-return validatedInput;
+const lose = () => {
+  /* computerScore++;
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`); */
+  computerDisplayScore.textContent = computerDisplayScore.value++;
 };
 
-/*this for loop determines the rounds played and declares the outcome of each 
-
-round by comparing the randomized computer value with the user input */
-
-  for (let i = 1; i < 6; i++) {
-    console.log(`Round: ${i}`);
-    const oneRound = (computerSelection, validatedPlayerSelection) => {
-      computerSelection = computerPlay();
-      console.log('Computer choice complete!');
-      validatedPlayerSelection = validate();
-      if ((computerSelection === 'Paper') && (validatedPlayerSelection === 
-
-'Rock')) {
-        alert('Paper covers Rock! Computer wins 1 point!');
-        lose();
-      } else if ((computerSelection === 'Rock') && (validatedPlayerSelection === 
-
-'Scissors')) {
-        alert('Rock breaks Scissors! Computer wins 1 point!');
-        lose();
-      } else if ((computerSelection === 'Scissors') && (validatedPlayerSelection 
-
-=== 'Paper')) {
-        alert('Scissors cuts Paper! Computer wins 1 point!');
-        lose();
-      } else if ((computerSelection === 'Paper') && (validatedPlayerSelection 
-
-=== 'Scissors')) {
-        alert('Scissors cuts Paper! You win 1 point!');
-        win();
-      } else if ((computerSelection === 'Rock') && (validatedPlayerSelection === 
-
-'Paper')) {
-        alert('Paper covers Rock! You win 1 point!');
-        win();
-      } else if ((computerSelection === 'Scissors') && (validatedPlayerSelection 
-
-=== 'Rock')) {
-        alert('Rock breaks Scissors! You win 1 point!');
-        win();
-      } else if (computerSelection === validatedPlayerSelection) {
-        alert(`${computerSelection} is the same as ${validatedPlayerSelection}.  
-        It's a tie!`);
-      } else { alert('Error! Please try again!'); }
-    };
-    oneRound();
-  }
-  console.log(`Final Score: Player: ${playerScore} Computer: ${computerScore}`);
-  if (playerScore > computerScore) { console.log("You win the game!"); } else { 
-
-console.log("Computer wins the game!"); }
+const oneRound = (computerSelection, validatedPlayerSelection) => {
+  computerSelection = computerPlay();
+  if ((computerSelection === 'Paper') && (validatedPlayerSelection === 'Rock')) {
+    alert('Paper covers Rock! Computer wins 1 point!');
+    lose();
+  } else if ((computerSelection === 'Rock') && (validatedPlayerSelection === 'Scissors')) {
+    alert('Rock breaks Scissors! Computer wins 1 point!');
+    lose();
+  } else if ((computerSelection === 'Scissors') && (validatedPlayerSelection === 'Paper')) {
+    alert('Scissors cuts Paper! Computer wins 1 point!');
+    lose();
+  } else if ((computerSelection === 'Paper') && (validatedPlayerSelection === 'Scissors')) {
+    alert('Scissors cuts Paper! You win 1 point!');
+    win();
+  } else if ((computerSelection === 'Rock') && (validatedPlayerSelection === 'Paper')) {
+    alert('Paper covers Rock! You win 1 point!');
+    win();
+  } else if ((computerSelection === 'Scissors') && (validatedPlayerSelection === 'Rock')) {
+    alert('Rock breaks Scissors! You win 1 point!');
+    win();
+  } else if (computerSelection === validatedPlayerSelection) {
+    alert(`${computerSelection} is the same as ${validatedPlayerSelection}. It's a tie!`);
+  } else { alert('Error! Please try again!'); }
 };
 
-game();
