cbc4ebc0765bb56b4dc2cd93236d4e55f6fcd151
Joey <55552636+JoeyJr37@users.noreply.github.com>
2019-11-15 11:26:16 -0600

    Added additional functionality
    Logic added to announce winner, hide game board upon completion & reset scores and reveal new game button

--- main.js
+++ main.js
@@ -7,7 +7,9 @@
  * Document Query Selectors
  */
 const gamePlayButton = document.querySelector("#gamePlayButton");
-const gameButton = document.querySelector("#game");
+const game = document.querySelector("#game");
+const gameButtons = document.querySelector("#gameButtons");
+const newGameButton = document.querySelector("#newGameButton");
 const playerDisplayScore = document.querySelector("#playerScore");
 const computerDisplayScore = document.querySelector("#computerScore");
 const rockButton = document.querySelector("#rockButton");
@@ -16,52 +18,68 @@ const scissorButton = document.querySelector("#scissorButton");
 
 let computer_score = 0;
 let player_score = 0;
+let winner;
 
 /**
  * Reveals Game Board
  */
 const revealGame = () => {
-  gameButton.style.visibility = "visible";
+  game.style.visibility = "visible";
   computerDisplayScore.innerHTML = computer_score;
   playerDisplayScore.innerHTML = player_score;
 };
+
+/**
+ * Reset Game
+ */
+const resetGame = () => {
+	computer_score = 0;
+	player_score = 0;
+	computerDisplayScore.innerHTML = computer_score;
+	playerDisplayScore.innerHTML = player_score;
+	newGameButton.style.visibility = "hidden";
+	let winnerText = document.querySelector("h2");
+	game.removeChild(winnerText);
+	gameButtons.style.visibility = "visible";
+}
 
 /**
  * Determine Winner of Game
  */
 const determineWinner = () => {
   if (player_score === 5) {
-    alert("Player 1 wins!");
+	let winner = "Player 1";
+	return winner;
   }
   else if (computer_score === 5) {
-    alert ("Computer wins!");
+	let winner = "Computer";
+	return winner;
   }
   else {
-    alert(`Player has ${player_score} and Computer has ${computer_score}`);
+    return false;
   };
 };
 
-/* function that sets validatedPlayerSelection to rock */
-const rockChoice = () => {
-  const validatedPlayerSelection = "Rock";
-  oneRound(computerPlay(), validatedPlayerSelection);
-  determineWinner();
+/**
+ * Announce Winner of Game
+ */
+const announceWinner = () => {
+if (!determineWinner()) {
+	alert (`Player has ${player_score} and Computer has ${computer_score}`);}
+else {
+	let winner = determineWinner();
+	let winnerText = document.createElement("h2");
+	winnerText.innerHTML = `${winner} is the winner!`;
+	winnerText.style.color = "red";
+	winnerText.style.fontSize = "32px";
+	game.appendChild(winnerText);
+	gameButtons.style.visibility = "hidden";
+	newGameButton.style.visibility = "visible";};
 };
 
-/* function that sets validatedPlayerSelection to paper */
-const paperChoice = () => {
-  const validatedPlayerSelection = "Paper";
-  oneRound(computerPlay(), validatedPlayerSelection);
-  determineWinner();
-};
-
-/* function that sets validatedPlayerSelection to scissors */
-const scissorChoice = () => {
-  const validatedPlayerSelection = "Scissors";
-  oneRound(computerPlay(), validatedPlayerSelection);
-  determineWinner();
-};
-
+/*
+ * Determine computer selection
+ */
 const computerPlay = () => {
   const computerValue = Math.floor((Math.random()) * 3);
   let computerChoice;
@@ -82,41 +100,42 @@ const computerPlay = () => {
 const win = () => {
   player_score += 1;
   playerDisplayScore.textContent = player_score;
+  announceWinner();
 };
 
 /*this function adds a point to the computer score after a player loss */
 const lose = () => {
   computer_score+= 1;
   computerDisplayScore.textContent = computer_score;
+  announceWinner();
 };
 
 /**
  * Play One Round
  * @param {String} computerSelection 
- * @param {String} validatedPlayerSelection 
+ * @param {String} playerSelection 
  */
-const oneRound = (computerSelection, validatedPlayerSelection) => {
-  computerSelection = computerPlay();
-  if ((computerSelection === 'Paper') && (validatedPlayerSelection === 'Rock')) {
+const oneRound = (computerSelection, playerSelection) => {
+  if ((computerSelection === 'Paper') && (playerSelection === 'Rock')) {
     alert('Paper covers Rock! Computer wins 1 point!');
     lose();
-  } else if ((computerSelection === 'Rock') && (validatedPlayerSelection === 'Scissors')) {
+  } else if ((computerSelection === 'Rock') && (playerSelection === 'Scissors')) {
     alert('Rock breaks Scissors! Computer wins 1 point!');
     lose();
-  } else if ((computerSelection === 'Scissors') && (validatedPlayerSelection === 'Paper')) {
+  } else if ((computerSelection === 'Scissors') && (playerSelection === 'Paper')) {
     alert('Scissors cuts Paper! Computer wins 1 point!');
     lose();
-  } else if ((computerSelection === 'Paper') && (validatedPlayerSelection === 'Scissors')) {
+  } else if ((computerSelection === 'Paper') && (playerSelection === 'Scissors')) {
     alert('Scissors cuts Paper! You win 1 point!');
     win();
-  } else if ((computerSelection === 'Rock') && (validatedPlayerSelection === 'Paper')) {
+  } else if ((computerSelection === 'Rock') && (playerSelection === 'Paper')) {
     alert('Paper covers Rock! You win 1 point!');
     win();
-  } else if ((computerSelection === 'Scissors') && (validatedPlayerSelection === 'Rock')) {
+  } else if ((computerSelection === 'Scissors') && (playerSelection === 'Rock')) {
     alert('Rock breaks Scissors! You win 1 point!');
     win();
-  } else if (computerSelection === validatedPlayerSelection) {
-    alert(`${computerSelection} is the same as ${validatedPlayerSelection}. It's a tie!`);
+  } else if (computerSelection === playerSelection) {
+    alert(`${computerSelection} is the same as ${playerSelection}. It's a tie!`);
   } else { alert('Error! Please try again!'); }
 };
 
@@ -124,6 +143,7 @@ const oneRound = (computerSelection, validatedPlayerSelection) => {
  * Event Listeners
  */
 gamePlayButton.addEventListener("click", revealGame);
-rockButton.addEventListener("click", rockChoice);
-paperButton.addEventListener("click", paperChoice);
-scissorButton.addEventListener("click", scissorChoice);
+rockButton.addEventListener("click", () => {oneRound(computerPlay(), "Rock")});
+paperButton.addEventListener("click", () => {oneRound(computerPlay(), "Paper")});
+scissorButton.addEventListener("click", () => {oneRound(computerPlay(), "Scissors")});
+newGameButton.addEventListener("click", resetGame);
